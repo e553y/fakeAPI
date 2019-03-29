5d5af33df6c2a46315adba1ea72e64c3723bf2b6
e553y <esey.iyasu@gmail.com>
2019-03-28 20:09:38 -0700

    update fetchParallelPromiseAll to use for loop

--- fetchMethods.js
+++ fetchMethods.js
@@ -4,21 +4,33 @@ const requestPromise = require('request-promise');
  * @param {Array} urls an array of valid URLs for resources to fetch
  * @returns {undefined} 
  */
-const fetchParallelPromiseAll = function(urls) {
+const fetchParallelPromiseAll = function (urls) {
   console.time('promiseAll')
-  let promises = urls.map((url) => requestPromise(url, {json: true}).then(response => response.results))
+  const promises = [];
+  for (let i = 0; i < urls.length; i++) {
+    //you could manipulate your urls here 
+
+    let modified = urls[i] + ''
+    return promises.push(requestPromise(modified, { json: true })
+      .then(response => response.results));
+  }
+
+
   Promise.all(promises)
     .then((resources) => {
       //you could manipulate your responses here 
-      console.log(resources); 
-      console.timeEnd('promiseAll')});
-  
+      console.log(resources);
+      console.timeEnd('promiseAll')
+    });
 }
-const fetchInOrder = async function(urls) {
+
+
+
+const fetchInOrder = async function (urls) {
   console.time('fetchInOrder');
   const resources = [];
-  for(let i = 0; i < urls.length; i++) {
-    let currResource = await requestPromise(urls[i], {json: true}).then(response => response.results);
+  for (let i = 0; i < urls.length; i++) {
+    let currResource = await requestPromise(urls[i], { json: true }).then(response => response.results);
     resources.push(currResource);
   }
   console.log(resources);
